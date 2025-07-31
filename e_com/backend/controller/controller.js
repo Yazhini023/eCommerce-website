8ebdf1168c9c2caaa9798ffecaf97219ee4668d5
Yazhini S <yazhinis023@gmail.com>
2025-07-31 16:11:39 +0530

    updation2

--- e_com/backend/controller/controller.js
+++ e_com/backend/controller/controller.js
@@ -1,3 +1,29 @@
+import { User } from "../model/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const registerUser= async(req,res) => {
-    
+    try {
+        const {name,email,password,contact} =req.body;
+        //Code to check that the email has already registered
+        let user=await User.findOne({email});
+        if(user){
+            res.status(400).json({
+                message:"User Email Already Exist",
+            });
+        }
+        //Code to convert the password to hashed password
+        const hashedPassword=await bcrypt.hash(password,10);
+        
+        //Code to Generate 6 digit OTP
+        const otp=Math.floor(Math.random()*1000000);
+
+        //Create signed activation token
+        const activationToken=jwt.sign({user,otp},process.env.ACTIVATION_SECRET,{
+            expiresIn:"5m",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message:error.message,
+        });
+    }
 };
