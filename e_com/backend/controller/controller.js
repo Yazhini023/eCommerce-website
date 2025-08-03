2069ff47bc13bde285cfce778102764317441183
Yazhini S <yazhinis023@gmail.com>
2025-08-03 23:12:25 +0530

    updation4

--- e_com/backend/controller/controller.js
+++ e_com/backend/controller/controller.js
@@ -39,12 +39,12 @@ export const registerUser= async(req,res) => {
         //Send email to user
         const message = `Please Verify your account using OTP you OTP is ${otp}`;
         await sendMail(email,"Welcome to eCommerce",message);
-        res.status(200).json({
+        return res.status(200).json({
             message:"OTP is send to your email",
             activationToken,
         });
     } catch (error) {
-        res.status(500).json({
+        return res.status(500).json({
             message:error.message,
         });
     }
@@ -73,8 +73,62 @@ export const verifyuser=async(req,res)=>{
         });
         return res.status(200).json({message:"User Registrartion Successful"});
     } catch (error) {
-        res.status(500).json({
+        return res.status(500).json({
             message:error.message,
         });
     }
 };
+
+//Login User
+export const loginUser=async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        //Check User Email Address
+
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                message:"Invalid Credentials",
+            });
+        }
+
+        //Check password
+        const matchpassword=await bcrypt.compare(password,user.password);
+        if(!matchpassword){
+            return res.status(400).json({
+                message:"Invalid Credentials",
+            });
+        }
+
+        //Generate Signed Token
+        const token=jwt.sign({_id:user.id},process.env.JWT_SECRET,{expiresIn:"15d"});
+        
+        //Exclude the assword field before sending the response
+        const {password:userPassword,...userDetails}=user.toObject();
+        return res.status(200).json({
+            message:"Welcome"+user.name,
+            token,
+            user:userDetails,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message,
+        });
+    }
+}
+
+//User Profile
+export const myProfile=async(req,res)=>{
+    try {
+        console.log(req.user);
+        const user=await User.findById(req.user._id).select("-password");
+        return res.status(200).json({
+            user,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message,
+        });
+    }
+}
--- e_com/backend/route/route.js
+++ e_com/backend/route/route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { registerUser, verifyuser } from "../controller/controller.js";
+import { loginUser, myProfile, registerUser, verifyuser} from "../controller/controller.js";
 
 
 const router=express.Router();
 
 router.post("/user/register", registerUser);
-router.post("/user/verify",verifyuser)
+router.post("/user/verify",verifyuser);
+router.post("/user/login",loginUser);
+router.get("/user/profile",myProfile);
 
 export default router;
