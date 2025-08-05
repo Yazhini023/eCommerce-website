69dac04d448e409bcdab2e207171874604ee7080
Yazhini S <yazhinis023@gmail.com>
2025-08-05 15:25:14 +0530

    updation7

--- e_com/backend/controller/Productcont.js
+++ e_com/backend/controller/Productcont.js
@@ -1,4 +1,5 @@
-import {Product} from "../model/Product.js";
+import { Product } from "../model/Product.js";
+import { rm } from "fs";
 
 //Add New Product
 export const createProduct=async(req,res) => {
@@ -36,4 +37,65 @@ export const createProduct=async(req,res) => {
             message: error.message,
         });
     }
+};
+
+//Fetches all product
+export const fetchAllProducts=async(req,res)=>{
+    try {
+        const product=await Product.find();
+        return res.status(200).json({
+            message:"List of Products",
+            product
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message,
+        });
+    }
+};
+
+//Fetch single Product
+export const fetchSinglelProduct=async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const product=await Product.findById(id);
+        return res.status(200).json({
+            message:"Product details",
+            product
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message,
+        });
+    }
+};
+
+//Delete Product
+export const deleteProduct=async(req,res)=>{
+   try {
+    //Check User Role
+    if(req.user.role!="admin"){
+        return res.status(403).json({
+            message:"Unauthorized Access",
+        });
+    }
+
+    const product=await Product.findById(req.params.id);
+    if(!product){
+        return res.status(403).json({
+            message:"Invalid Product Details",
+        });
+    }
+
+    rm(product.image,() => {
+        console.log("Image Deleted");
+    });
+
+    await Product.deleteOne();
+    return res.json({message:"Product Details Deleted Successfully",});
+   } catch (error) {
+    return res.status(500).json({
+        message:error.message,
+    });
+   }
 };
--- e_com/backend/route/Productroute.js
+++ e_com/backend/route/Productroute.js
@@ -1,10 +1,13 @@
 import express from"express";
 import { isAuth } from "../middleware/authen.js";
 import { uploadFiles } from "../middleware/productmid.js";
-import { createProduct } from "../controller/Productcont.js";
+import { createProduct, deleteProduct, fetchAllProducts, fetchSinglelProduct } from "../controller/Productcont.js";
 
 const router = express.Router();
 
 router.post("/product/new",isAuth, uploadFiles, createProduct);
+router.get("/product/all-products",fetchAllProducts);
+router.get("/product/single/:id",fetchSinglelProduct);
+router.delete("/product/:id",isAuth, deleteProduct);
 
 export default router;
